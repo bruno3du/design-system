b47b44806ed164dee1d13e478a73cfaf0e75d22b
bruno3du <dev@wisefy.io>
2023-05-07 15:33:00 -0300

    feat: created components and its stories

---
+++ packages/react/src/components/Text.tsx
@@ -0,0 +1,29 @@
+import { ComponentProps } from '@stitches/react';
+import { styled } from '../styles';
+import { fontSizes } from '@bruno-ui/tokens';
+import { ElementType } from 'react';
+
+export const Text = styled('p', {
+    fontFamily: '$default',
+    lineHeight: '$base',
+    margin: 0,
+    color: '$gray500',
+    variants: {
+        size: Object.keys(fontSizes).reduce<Record<string, { fontSize: string }>>(
+            (acc, cur) => {
+                acc[cur] = {
+                    fontSize: `$${cur}`,
+                }
+                return acc
+            },
+            {},
+        ),
+    },
+    defaultVariants: {
+        size: 'md',
+    },
+})
+
+export interface TextProps extends ComponentProps<typeof Text> {
+    as?: ElementType
+}
