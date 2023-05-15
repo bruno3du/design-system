cc940c7382700d9f7195372f29a7c07f9174d761
bruno3du <dev@wisefy.io>
2023-05-14 21:11:58 -0300

    fix: trying to make doc work

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -1,26 +1,33 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
-const config = {
-  stories: ["../src/pages/**/*.mdx", "../src/docs/**/*.mdx", "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"],
+
+module.exports = {
+  stories: [
+    "../src/pages/**/*.stories.mdx",
+    "../src/stories/**/*.stories.tsx"
+  ],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "@storybook/addon-a11y"
   ],
-  framework: "@storybook/react",
+  framework: "@storybook/react-vite",
+
   docs: {
     autodocs: true,
     defaultName: 'Documentation',
   },
+  // features: {
+  //   storyStoreV7: true
+  // },
   core: {
-    builder: "@storybook/builder-vite"
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
   },
-  viteFinal: (config, { configType }) => {
+  viteFinal: async (config, { configType }) => {
     if (configType === 'PRODUCTION') {
       config.base = '/design-system/'
     }
 
-    return config;
+    return config
   }
-};
-export default config;
+}
