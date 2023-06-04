9f3a8e02d0b91e3ecb5a9b7668fa73e8b780fc37
bruno3du <dev@wisefy.io>
2023-06-04 14:18:31 -0300

    chore: add new test to colors tokens

--- packages/tokens/src/colors.ts
+++ packages/tokens/src/colors.ts
@@ -1,7 +1,7 @@
 export const colors = {
     white: '#FFF',
     black: '#000',
-  
+
     gray100: '#E1E1E6',
     gray200: '#A9A9B2',
     gray400: '#7C7C8A',
@@ -15,4 +15,5 @@ export const colors = {
     ignite500: '#00875F',
     ignite700: '#015F43',
     ignite900: '#00291D',
+    test: '#FFF'
   }
